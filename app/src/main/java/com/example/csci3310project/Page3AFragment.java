8ccdec94f28708fb981691fc83c50d86d560156f
Jacky <jackychen924@gmail.com>
2025-03-28 19:06:10 +0800

    fix: revert extra changes

--- app/src/main/java/com/example/csci3310project/Page3AFragment.java
+++ app/src/main/java/com/example/csci3310project/Page3AFragment.java
@@ -11,6 +11,6 @@ public class Page3AFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.section1_dashboard, container, false);
+        return inflater.inflate(R.layout.page1a, container, false);
     }
 }
