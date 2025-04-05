17484c86502a6cc3e240b82e2098c3b6852de9db
MCY-12 <mcymcy12@gmail.com>
2025-04-05 20:01:58 +0800

    head tilt angle ver 1
    proof of concept works, but accuracy problems on the AI end which is used to determine the neck angle based on the user's facial features

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,6 +28,9 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+
+        // Idk if this
+        isCoreLibraryDesugaringEnabled = true
     }
 }
 
@@ -36,8 +39,29 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.room.runtime)
+    implementation(libs.camera.lifecycle)
+    implementation(libs.camera.view)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     annotationProcessor(libs.room.compiler)
+
+    // CameraX
+    implementation("androidx.camera:camera-core:1.3.3")
+    implementation("androidx.camera:camera-camera2:1.3.3")
+    implementation("androidx.camera:camera-lifecycle:1.3.3")
+    implementation("androidx.camera:camera-view:1.3.3")
+
+    // ML Kit Face Detection
+    //implementation("com.google.android.gms:play-services-mlkit-face-detection:17.1.0")
+    implementation ("com.google.mlkit:face-detection:16.1.7")
+
+    // -
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-common-java8:2.7.0")
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
