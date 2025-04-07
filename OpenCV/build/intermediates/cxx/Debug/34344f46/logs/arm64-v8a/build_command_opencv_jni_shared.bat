2e1ed7396479e50d2113ea277c5acbfb6edce9d4
Jacky <jackychen924@gmail.com>
2025-04-07 21:30:53 +0800

    Remove OpenCV/build from Git tracking

--- OpenCV/build/intermediates/cxx/Debug/34344f46/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\User\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\User\\CSCI3310-Project\\OpenCV\\.cxx\\Debug\\34344f46\\arm64-v8a" ^
-  opencv_jni_shared
